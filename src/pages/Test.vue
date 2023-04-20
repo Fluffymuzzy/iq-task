df3ab1999eeefc15013b7d10bd35c5c822069dd9
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-04-20 19:29:09 +0300

    done Quiz functionality and add Progress bar

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,26 +1,28 @@
 <template>
     <section class="test__wrapper" v-if="!testCompleted">
-        <div class="test__progress"></div>
+        <div class="test__progress" :style="{ background: progressBar }">
+        </div>
         <p class="test__question">{{ getCurrentQuestion.question }}</p>
+        <div class="test__image" v-if="getCurrentQuestion.image">
+            <img :src="getCurrentQuestion.image" alt="image" />
+        </div>
         <div class="test__options">
             <label v-for="(option, index) in getCurrentQuestion.options" :key="index" :class="`option ${getCurrentQuestion.selected == index ? index == getCurrentQuestion.answer ? 'correct'
                 : 'correct' : ''} ${getCurrentQuestion.selected != null && index != getCurrentQuestion.selected ? 'disabled' : ''
                 }`">
                 <input type="radio" :name="getCurrentQuestion.index" :value="index" v-model="getCurrentQuestion.selected"
                     :disabled="getCurrentQuestion.selected" @change="setAnswer">
-
-        
-                <div v-if="getCurrentQuestion.isColorQuestion" class="color-option"
+                <div v-if="getCurrentQuestion.isColorQuestion" class="color__option"
                     :style="{ backgroundColor: option.value }"></div>
-
-                
                 <span class="test__option" v-else>{{ option }}</span>
             </label>
         </div>
-        <YellowButton buttonText="Далее" buttonClass="test__btn" @click="nextQuestion"
-            :disabled="!getCurrentQuestion.selected" />
+        <Button buttonText="Далее" buttonClass="test__btn" @click="nextQuestion" :disabled="!getCurrentQuestion.selected" />
     </section>
 
+
+    <section v-else-if="loading">
+    </section>
 
     <section v-else>
         <h2>done!</h2>
@@ -29,11 +31,8 @@
 
 <script setup>
 
-
 import { ref, computed } from "vue"
-import YellowButton from '../components/YellowButton.vue';
-
-
+import Button from '../components/Button.vue';
 
 const questions = ref([
     {
@@ -45,6 +44,9 @@ const questions = ref([
             "Женщина"
         ],
         selected: null,
+        image: null,
+
+
     },
     {
 
@@ -57,6 +59,8 @@ const questions = ref([
             "От 36"
         ],
         selected: null,
+        image: null,
+
     },
     {
 
@@ -70,6 +74,8 @@ const questions = ref([
             "Хижина"
         ],
         selected: null,
+        image: null,
+
     },
     {
 
@@ -84,6 +90,8 @@ const questions = ref([
             "77"
         ],
         selected: null,
+        image: null,
+
     },
     {
 
@@ -101,11 +109,99 @@ const questions = ref([
             { value: "#46B2AC" },
         ],
         selected: null,
-    }
+        image: null,
+    },
+    {
+
+        question: "Отдохните пару секунд и выберите цвет, который сейчас наиболее Вам приятен:",
+        answer: 9,
+        options: [
+            { value: "#A8A8A8" },
+            { value: "#46B2AC" },
+            { value: "#A95403" },
+            { value: "#00A701" },
+            { value: "#000000" },
+            { value: "#F60100" },
+            { value: "#850068" },
+            { value: "#FDFF19" },
+            { value: "#0000A9" },
+        ],
+        selected: null,
+    },
+    {
+
+        question: "Какой из городов лишний? (Москва)",
+        answer: 6,
+        options: [
+            "Вашингтон",
+            "Лондон",
+            "Париж",
+            "Нью-Йорк",
+            "Москва",
+            "Оттава"
+        ],
+        selected: null,
+        image: null,
+
+    },
+    {
+        question: "Выберите правильную фигуру из четырёх пронумерованных.",
+        answer: 4,
+        options: [
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+        selected: null,
+        image: "../../public/images/test/test.png",
+
+    },
+    {
+
+        question: "Вам привычнее и важнее",
+        answer: 3,
+        options: [
+            "Наслаждаться каждой минутой проведенного времени",
+            "Быть устремленными мыслями в будущее",
+            "Учитывать в ежедневной практике прошлый опыт",
+        ],
+        selected: null,
+        image: null,
+
+    },
+    {
+        question: "Какое определение, по-Вашему, больше подходит к этому геометрическому изображению: ",
+        answer: 3,
+        options: [
+            "Оно остроконечное",
+            "Оно устойчиво",
+            "Оно-находится в состоянии равновесия",
+        ],
+        selected: null,
+        image: "../../public/images/test/test2.png",
+
+    },
+    {
+        question: "Вставьте подходящее число",
+        answer: 6,
+        options: [
+            "34",
+            "36",
+            "53",
+            "44",
+            "66",
+            "42"
+        ],
+        selected: null,
+        image: "../../public/images/test/test3.png",
+
+    },
 ])
 
 const testCompleted = ref(false)
 const currentQuestion = ref(0)
+const loading = ref(false);
 
 const getCurrentQuestion = computed(() => {
     let question = questions.value[currentQuestion.value];
@@ -115,9 +211,17 @@ const getCurrentQuestion = computed(() => {
         question.isColorQuestion = true;
     }
 
-
     return question;
 })
+
+const progressWidth = computed(() => {
+    return `${((currentQuestion.value + 1) / questions.value.length) * 100}%`;
+});
+
+const progressBar = computed(() => {
+    return `linear-gradient(to right, #3BDE7C ${progressWidth.value}, #F2F3F3 ${progressWidth.value})`;
+});
+
 
 const setAnswer = (e) => {
     questions.value[currentQuestion.value].selected = e.target.value;
@@ -129,19 +233,40 @@ const nextQuestion = () => {
     if (currentQuestion.value < questions.value.length - 1) {
         currentQuestion.value++
     } else {
-        testCompleted.value = true;
+        loading.value = true;
+        setTimeout(() => {
+            loading.value = false;
+            testCompleted.value = true;
+        }, 3000);
     }
 }
+
+
 
 
 </script>
 
 <style>
-.color-option {
-    width: 30px;
+.loading {
+    color: #fff;
+}
+
+.test__image {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.test__image img {
+    max-width: 100%;
+    max-height: 100%;
+}
+
+.color__option {
+    width: 100%;
     height: 30px;
     margin: 5px;
-    border-radius: 50%;
     cursor: pointer;
 }
 
@@ -149,26 +274,41 @@ const nextQuestion = () => {
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-image: url(../assets/images/main_bg_lg.png);
-    height: 100vh;
+    background-image: url(../../public/images/main_bg_lg.png);
+    min-height: 100vh;
 }
 
 .test__progress {
     margin-top: 17px;
     width: 260px;
     height: 11px;
-    background: #F2F3F3;
+    background-color: #F2F3F3;
     mix-blend-mode: normal;
     opacity: 0.59;
     border-radius: 10.5px;
 }
 
+.test__progress.completed {
+    background-color: #00FF00;
+}
+
+
 
 .test__option {
     color: #fff;
+    font-family: 'PT Serif';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 48px;
+    letter-spacing: 0.05em;
+    text-transform: capitalize;
+    margin-left: 15px;
 }
 
 .test__question {
+    margin-top: 20px;
+    margin-bottom: 90px;
     width: 237px;
     height: 26px;
     font-family: 'PT Serif';
@@ -181,5 +321,41 @@ const nextQuestion = () => {
     color: #fff;
 }
 
-.test__options {}
+.test__options {
+    margin-top: 25px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 320px;
+}
+
+.option {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-bottom: 5px;
+    background-color: #1e2020;
+    border-radius: 0.05rem;
+}
+
+.option.correct {
+    background-color: #FFC700;
+}
+
+.option.disabled {
+    opacity: 0.5;
+}
+
+.option:hover {
+    background-color: #FFC700;
+    border-radius: 0.05rem;
+}
+
+.option:last-of-type {
+    margin-bottom: 0;
+}
+
+.option input {
+    display: none;
+}
 </style>
